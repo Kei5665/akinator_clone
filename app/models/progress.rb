ed2449a2d5e8c52749d0571b9d3ba52ece9d1f4c
Kei <kei@KeinoMacBook-Air.local>
2022-03-10 15:26:28 +0900

    Add: ゲームの中で現在の質問が何番目の質問なのかを記録するメソッドを記述。

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,4 +1,17 @@
 class Progress < ApplicationRecord
   belongs_to :game
   belongs_to :question
+
+  # 同じゲーム内で1から連番を発行する
+  def assign_sequence
+    # 新たに発番する番号(next_sequence)を1で初期化
+    next_sequence = 1
+    if game.present?
+      all_progress = game.progresses
+      if all_progress.count > 0
+        next_sequence = all_progress.maximum(:sequence) + 1
+      end
+    end
+    self.sequence = next_sequence
+  end
 end
