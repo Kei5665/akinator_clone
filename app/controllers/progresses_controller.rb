17b677d3deb93560b0c43e43197d0fb28e505d41
Kei <kei@KeinoMacBook-Air.local>
2022-03-13 17:40:47 +0900

    Add: 質問がなくなったらギブアップする

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -13,6 +13,19 @@ class ProgressesController < ApplicationController
     progress.assign_sequence
     progress.save!
 
+    next_question = Question.next_question(current_game)
+
+    # 質問がなくなったらギブアップする
+    if next_question.blank?
+
+      current_game.status = 'finished'
+      current_game.result = 'incorrect'
+      current_game.save!
+
+      redirect_to give_up_game_path(current_game)
+      return
+    end
+
     redirect_to new_game_progresses_path(current_game)
   end
 
