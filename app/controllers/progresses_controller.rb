be93198c9f74a91a29018403890e7671c7992155
Kei <kei@KeinoMacBook-Air.local>
2022-03-14 20:30:26 +0900

    Add: Progressesコントローラーで絞り込みアルゴリズムを実行する

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -13,6 +13,9 @@ class ProgressesController < ApplicationController
     progress.assign_sequence
     progress.save!
 
+    # 絞り込みを実行
+    @extract_comics = ExtractionAlgorithm.new(current_game).compute
+
     next_question = Question.next_question(current_game)
 
     # 質問がなくなったらギブアップする
