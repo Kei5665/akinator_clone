f36edcd0586b60015c7f3dd55c3c0ba9e73c42f0
Kei <kei@KeinoMacBook-Air.local>
2022-03-22 01:43:13 +0900

    Add: 絞り込みアルゴリズムにジャンルでの絞り込みを追加します

--- app/algorithms/extraction_algorithm.rb
+++ app/algorithms/extraction_algorithm.rb
@@ -17,6 +17,8 @@ class ExtractionAlgorithm
       question = progress.question
 
       case question.algorithm
+      when 'genre_match'
+        genre_match(progress)
       when 'serialization_end'
         serialization_end?(progress)
       else
@@ -39,6 +41,18 @@ class ExtractionAlgorithm
 
     if progress.negative_answer?
       @query = @query.where("comics.serialization_end_year is null")
+    end
+
+  end
+
+  def genre_match(progress)
+
+    if progress.positive_answer?
+      @query = @query.where("comics.genre like ?", "%#{progress.question.eval_value}%")
+    end
+
+    if progress.negative_answer?
+      @query = @query.where.not("comics.genre like ?", "%#{progress.question.eval_value}%")
     end
 
   end
